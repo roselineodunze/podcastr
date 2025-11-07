ef60be877d93a23f04cf7283c68f90352ac3d6ed
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2025-11-07 19:41:26 +0100

    homepage made responsive

--- components/homepage/LatestPodcastListing.jsx
+++ components/homepage/LatestPodcastListing.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Image from "next/image";
+import { Tooltip } from "../ui/tooltip";
 
 const LatestPodcastListing = ({ index }) => {
   return (
-    <div className={`flex items-center ${
-        index !== 4 ? 'border-b-2 border-gray-800' : ''
-      }`}>
-      <div className="flex items-center gap-5 w-1/2 my-2">
+    <div
+      className={`flex items-center gap-5 sm:gap-0 ${
+        index !== 4 ? "border-b-2 border-gray-800" : ""
+      }`}
+    >
+      <div className="flex items-center gap-5 w-[70%] sm:w-1/2 my-2">
         <h3 className="text-white-1">{index}</h3>
         <div className="flex items-center my-2 gap-2 h-10">
           <div className="w-10 rounded-sm h-full">
@@ -25,17 +28,52 @@ const LatestPodcastListing = ({ index }) => {
       </div>
       <div className="flex items-center w-1/2 justify-between my-2">
         <div className="flex items-center gap-2">
-          <Image
-            src="/icons/headphone.svg"
-            alt="app-logo"
-            width={23}
-            height={23}
-          />
-          <h1 className="text-white-1 text-[14px]">143,809</h1>
+          <Tooltip
+            content="143,809"
+            positioning={{
+              placement: "top",
+              offset: { mainAxis: 4, crossAxis: 4 },
+            }}
+            openDelay={400}
+            closeDelay={100}
+            contentProps={{
+              className:
+                "bg-black-2 text-sm px-2 py-1 rounded-md block sm:hidden",
+            }}
+            unstyled
+          >
+            <Image
+              src="/icons/headphone.svg"
+              alt="app-logo"
+              width={23}
+              height={23}
+            />
+          </Tooltip>
+          <h1 className="text-white-1 text-[14px] hidden sm:block">143,809</h1>
         </div>
         <div className="flex items-center gap-2">
-          <Image src="/icons/watch.svg" alt="app-logo" width={23} height={23} />
-          <h1 className="text-white-1 text-[14px]">1:04:23</h1>
+          <Tooltip
+            content="1:04:23"
+            positioning={{
+              placement: "top",
+              offset: { mainAxis: 4, crossAxis: 4 },
+            }}
+            openDelay={400}
+            closeDelay={100}
+            contentProps={{
+              className:
+                "bg-black-2 text-sm px-2 py-1 rounded-md block sm:hidden",
+            }}
+            unstyled
+          >
+            <Image
+              src="/icons/watch.svg"
+              alt="app-logo"
+              width={23}
+              height={23}
+            />
+          </Tooltip>
+          <h1 className="text-white-1 text-[14px] hidden sm:block">1:04:23</h1>
         </div>
         <Image
           src="/icons/three-dots.svg"
